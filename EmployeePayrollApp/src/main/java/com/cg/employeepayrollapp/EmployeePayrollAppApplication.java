984dec8de1a0bb21130952af7e3450b8cc533c4c
dev01997D <devnandan01997@gmail.com>
2020-12-13 22:23:24 +0530

    Added: Lombok slf4j logging is used with different level of logging in diffrent branch like DEV, PROD etc

--- EmployeePayrollApp/src/main/java/com/cg/employeepayrollapp/EmployeePayrollAppApplication.java
+++ EmployeePayrollApp/src/main/java/com/cg/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -13,6 +13,7 @@ public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
 		
 		ApplicationContext context=SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		log.info("Employee Payroll App Started");
+		log.info("Employee Payroll App Started in {} Environment",
+				  context.getEnvironment().getProperty("environment"));
 	}
 }
